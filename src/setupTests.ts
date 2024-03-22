2bb6b159d1863f552b38575fdcc65e39cdcad952
talal <talalabbas84@gmail.com>
2024-03-22 12:57:44 -0400

    (talal): fix jest testing bug

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,16 +1,31 @@
 import '@testing-library/jest-dom';
-
+// @ts-ignore
+declare var global: {
+  chrome: {
+    runtime: {
+      sendMessage: jest.Mock<any, any>,
+      onMessage: {
+        addListener: jest.Mock<any, any>
+      }
+    },
+    storage: {
+      local: {
+        get: jest.Mock<any, any>,
+        set: jest.Mock<any, any>,
+        remove: jest.Mock<any, any>
+      }
+    }
+  }
+};
 
 global.chrome = {
   runtime: {
     sendMessage: jest.fn(),
-    // @ts-ignore
     onMessage: {
       addListener: jest.fn()
     }
   },
   storage: {
-    // @ts-ignore
     local: {
       get: jest.fn(),
       set: jest.fn(),
