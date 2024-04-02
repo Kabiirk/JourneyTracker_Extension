377b154079ca45ae0dfbb95c51656a448cfda144
Kabiir <kabiirk@gmail.com>
2024-04-02 05:14:13 -0400

    (Kabiir): Configured Testing with Puppeteer

--- src/__test__/journeyList.test.tsx
+++ src/__test__/journeyList.test.tsx
@@ -1,11 +1,81 @@
+/**
+ * @jest-environment jsdom
+ */
 // import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import JourneyList from '../components/JourneyList';
+import { IJourney } from '../hooks/useJourney';
 
-test('renders JourneyList component', () => {
-  // render(<JourneyList />);
+describe('JourneyList component', () => {
+  const mockAddNewJourney = jest.fn();
+  const mockUpdateSelectedJourney = jest.fn();
 
-  expect(true).toBe(true);
-  // // Ensure that the label 'Journeys' is present
-  // expect(screen.getByLabelText('Journeys')).toBeInTheDocument();
+  // const journeys = [
+  //   { id: '1', label: 'Journey 1' },
+  //   { id: '2', label: 'Journey 2' },
+  // ];
+
+  const j:IJourney = {
+    id: "id_string",
+    label: "label_string",
+    recordedTexts:[{
+          text: "text_string",
+          url: "url_string",
+          createdAt: "createdAt_string",
+        }],
+    createdAt: "string",
+  }
+
+  const selectedJourney = j;
+
+  test('renders correctly', () => {
+    const { getByLabelText, getByText } = render(
+      <JourneyList
+        addNewJourney={mockAddNewJourney}
+        journeys={[selectedJourney]}
+        updateSelectedJourney={mockUpdateSelectedJourney}
+        selectedJourney={selectedJourney}
+      />
+    );
+    // expect(getByTestId('SaveAltIcon')).toBeInTheDocument();
+    expect(getByLabelText('Clear')).toBeInTheDocument();
+    expect(getByText('Journeys')).toBeInTheDocument();
+    // expect(getByText('label_string')).toBeInTheDocument();
+  });
+
+  test('displays create new journey text field when there are no journeys', () => {
+    const { getByLabelText } = render(
+      <JourneyList
+        addNewJourney={mockAddNewJourney}
+        journeys={[selectedJourney]}
+        updateSelectedJourney={mockUpdateSelectedJourney}
+        selectedJourney={selectedJourney}
+      />
+    );
+
+    expect(getByLabelText('Clear')).toBeInTheDocument();
+  });
+
+  test('calls addNewJourney when save button is clicked', () => {
+    const { getByLabelText } = render(
+      <JourneyList
+        addNewJourney={mockAddNewJourney}
+        journeys={[selectedJourney]}
+        updateSelectedJourney={mockUpdateSelectedJourney}
+        selectedJourney={selectedJourney}
+      />
+    );
+
+    // const saveButton = getByText('Save');
+    // const inputField = getByLabelText('Create new journey');
+
+    // fireEvent.change(inputField, { target: { value: 'New Journey' } });
+    // fireEvent.click(saveButton);
+
+    // expect(mockAddNewJourney).toHaveBeenCalledWith('New Journey');
+    // expect(inputField.value).toBe('');
+
+    expect(getByLabelText('Clear')).toBeInTheDocument();
+  });
 });
-
-export { }
---
+++ src/__test__/puppeteer.test.tsx
@@ -0,0 +1,28 @@
+import * as puppeteer from "puppeteer";
+import '@testing-library/jest-dom';
+
+// Replace with your absolute path of dist/ 
+const EXTENSION_PATH = 'C:/Users/kabii/Desktop/FOLDERS/UWATERLOO/2 - Sem 2 - Winter\'24/ECE 651/Project/JourneyTracker_Extension/dist';
+const EXTENSION_ID = 'nddhdlfndkgohnpbegaopeglgkdahkpa';
+
+let browser : any;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      args: [
+        `--disable-extensions-except=${EXTENSION_PATH}`,
+        `--load-extension=${EXTENSION_PATH}`
+      ]
+    });
+  });
+  
+afterEach(async () => {
+  await browser.close();
+  browser = undefined;
+});
+
+test('popup renders correctly', async () => {
+  const page = await browser.newPage();
+  await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+})
