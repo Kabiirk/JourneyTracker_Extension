221e10ab5f306155741cfe6ce95efc21df24b380
Abhith Krishna <abyk476@gmail.com>
2024-04-05 11:39:20 -0400

    (Abhith) Added create Journey flow in e2e test

--- src/__puppeteerTests__/puppeteer.test.tsx
+++ src/__puppeteerTests__/puppeteer.test.tsx
@@ -173,71 +173,6 @@ describe('JourneyList component', () => {
     await page.close();
   });
 
-  test('Record text functionality', async () => {
-    const page = await browser.newPage();
-    await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
-    const client = await page.target().createCDPSession();
-    await client.send('Network.clearBrowserCookies');
-    await client.send('Network.clearBrowserCache');
-    await setTimeout(1000);
-    // Set session cookie
-    await page.setCookie({
-      name: 'next-auth.session-token',
-      // value: 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..mDsS-b5w8dRluy1Z.U1vZK40LsUNxWnQl8Exzon9PFQk0iPM_SkPGL0vCn_iPiM_v2eUMJi4C3kXzVNyBTqQ8TikxGViLqn44Ee_lhaFXCjW5EI7fFzXgVcAmkLpLr_5rXdjiGqrOoWrQTMv6k-QNGLp_YnK6Uq38f3Bsjlp4ja2M2zlM4R8fYMaGG3bM7fIHCg5ZTwDotvGNuR3sVmAf6ENlJAWZsM4c36at73_YcmywPhpObnCdsw.50IavPtQBal_eomSrMb11w',
-      value: 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..Bev4xnsUF3dg9NoJ.Y4YraD3vhPL9yryK4Lbe_vJ_9Z3ouG0DvwKS2B8LXLdEO0t0VNNYdiRbRm2TeCyAm4oay1yB0i8YTkkxz6LUAC4bleAUvg0FT1qzP_W82NW20lt3j29jAae3mu83bzN50N_4S6khVwzDAw1Yy5D6dxFD1ggZfN0RK4Xb_Gkbfe9lzn9rcSV0slrlHSgXSFctuBsPjjlbiiyyWxgORcg4_InsHsNN4_tzQ5XXmw.Re1Wm04ZOHBEV8iYLyN1Mg',
-      domain: 'localhost:3000', // Set your domain here
-      path: '/', // Set cookie path
-      httpOnly: true, // Set HTTP only flag if needed
-      secure: true, // Set secure flag if needed (for HTTPS)
-      sameSite: 'Lax' // Set sameSite attribute if needed
-    });
-  
-    // click and wait for navigation
-    const [] = await Promise.all([
-      new Promise((resolve, reject) => {
-        browser.once('targetcreated', (target: Target) => { resolve(target.url()); });
-      }),
-      page.click('.MuiButtonBase-root'),
-    ]);
-    
-    await page.bringToFront();
-    // await page.reload()
-    const page2 = await browser.newPage();
-    await page2.goto('https://stackoverflow.com/questions/71942822/materialui-v5-how-to-style-autocomplete-options');
-    await setTimeout(3000);
-
-    await page2.waitForSelector('p');
-
-    await page2.evaluate(() => {
-      const paragraph = document.querySelector('p'); // Assuming you want to select the first paragraph
-      paragraph.scrollIntoView({ behavior: 'smooth', block: 'center' });
-
-      const range = document.createRange();
-      range.selectNode(paragraph);
-      const selection = window.getSelection();
-      selection.removeAllRanges();
-      selection.addRange(range);
-    });
-    await setTimeout(2000);
-
-
-    // Scroll into the highlighted text
-    // await page.evaluate(() => {
-    //   const paragraph = document.querySelector('p'); // Assuming you want to select the first paragraph
-    //   paragraph.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    // });
-  
-    // await setTimeout(6000);
-
-
-
-    // await page.bringToFront();
-
-    await page.close();
-    await page2.close();
-  });
-
-
   test('Create a new journey', async () => {
     const page = await browser.newPage();
     await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
@@ -273,28 +208,27 @@ describe('JourneyList component', () => {
     await page.click(buttonSelector);
     await setTimeout(1000);
 
-    const inputSelector = '.MuiInputBase-root'; // Replace with the actual class name
-    await page.waitForSelector('.MuiInputBase-root', { timeout: 5000 }); // Wait for the input to appear
+    const inputSelector = '.MuiInputBase-input'; // Replace with the actual class name
+    await page.waitForSelector('.MuiInputBase-input', { timeout: 5000 }); // Wait for the input to appear
 
-    await page.type(inputSelector, ' Journey e2e');
-    await setTimeout(3000);
-    await page.waitForSelector('.MuiButtonBase-root');
+    await page.type(inputSelector, 'Journey e2e');
+    await setTimeout(1000);
+    await page.waitForSelector(buttonSelector);
+    await page.click(buttonSelector);
+    await setTimeout(1000);
+    const [] = await Promise.all([
+      page.click('.MuiAutocomplete-endAdornment').then(() => {
+      }).catch((error:any) => {
+        console.error('Error clicking on element:', error);
+      })
+    ]);
 
-  // Find all buttons with the class 'MuiButtonBase-root'
-    // const buttons = await page.$$('.MuiButtonBase-root');
-    // console.log(buttons);
-    // await buttons[1].click();
-    // await setTimeout(4000);
 
-    // const buttonSelector = '[data-testid="your-button-testid"]'; // Replace with the actual data-testid value
-    // await page.waitForSelector(buttonSelector);
-    // await page.click(buttonSelector);
-    
+
+
+    await setTimeout(4000);
 
 
     await page.close();
   });
-
-   
-
 });
